bd6777a780ae38d06b88e657247bfaa75bf83e58
Fatih Islamovski <116791165+fatihisla@users.noreply.github.com>
2023-04-28 09:12:10 +0200

    Add files via upload
    prima versione

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SQLite;
+
+
+
+Console.WriteLine("Hello, DBWorld!");
+
+    //connessione a db
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtist = cn1.Query<Artist>("select * from artists ");
+Console.WriteLine($"in questa tabella ci sono {tblArtist.Count} record!");
+
+
+//------------------------
+public class Artist
+{
+    public int ArtistId { get; set; }
+    public string Name { get; set; }
+
+}
